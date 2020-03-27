9470e76e26f963eb267fbd9e248925f54b07c584
Riska Kurnia Dewi <riska.kurnia81@ui.ac.id>
2020-03-27 19:05:31 +0700

    2

--- src/main/java/com/apap/tu07/rest/Setting.java
+++ src/main/java/com/apap/tu07/rest/Setting.java
@@ -1,5 +1,7 @@
 package com.apap.tu07.rest;
 
 public class Setting {
-	final public static String pilotUrl = "https://2d8f7956-9c7d-4681-9b33-5afa1b02e458.mock.pstmn.io";
+    final public static String pilotUrl = "https://2d8f7956-9c7d-4681-9b33-5afa1b02e458.mock.pstmn.io";
+    final public static String airportUrl = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete";
+    final public static String airportApikey = "0LzD34hwtPms4VelaAAIXHCA95xyUN0E";
 }
